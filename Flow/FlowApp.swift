5682b468144013146592835264acc7df5989fa21
Manuel Schulze <m.schulze@nanogiants.de>
2022-01-26 21:41:44 +0100

    Initial Commit

---
+++ Flow/FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowApp.swift
+//  Flow
+//
+//  Created by Manuel Schulze on 26.01.22.
+//
+
+import SwiftUI
+
+@main
+struct FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
