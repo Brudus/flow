2cc614d5b7e9241576deace17dbbe65ea9af5055
Manuel Schulze <m.schulze@nanogiants.de>
2022-01-27 13:52:54 +0100

    refactor: add color assets

--- Flow/ContentView.swift
+++ Flow/ContentView.swift
@@ -12,8 +12,8 @@ struct ContentView: View {
         ZStack {
             LinearGradient(
                 colors: [
-                    Color(red: 91/255.0, green: 195/255.0, blue: 190/255.0),
-                    Color(red: 75/255.0, green: 26/255.0, blue: 111/255.0)
+                    Color("CountdownBGStart"),
+                    Color("CountdownBGEnd")
                 ],
                 startPoint: .topTrailing,
                 endPoint: .bottomLeading
@@ -46,10 +46,10 @@ struct ContentView: View {
                 .overlay(
                     ZStack {
                         Circle()
-                            .stroke(Color(.sRGB, white: 1.0, opacity: 0.05), lineWidth: 6)
+                            .stroke(Color("CountdownRingBG"), lineWidth: 6)
                         Circle()
                             .trim(from: 0.0, to: CGFloat(0.8))
-                            .stroke(Color(.sRGB, red: 75/255.0, green: 26/255.0, blue: 111/255.0, opacity: 0.7), lineWidth: 6)
+                            .stroke(Color("CountdownRingFG"), lineWidth: 6)
                         
                         
                     }
